41740b94373a6bd0aac95c43f0bd40d3f58ae694
rasheedkotoor <rasheedkmkotoor@gmail.com>
2021-07-05 10:48:43 +0530

    add secret key

--- StudentsCircle/settings.py
+++ StudentsCircle/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+SECRET_KEY = config("SECRET_KEY")
+
 ALLOWED_HOSTS = ["134.209.153.105",]
 
 ROOT_URLCONF = f'{config("PROJECT_NAME")}.urls'
